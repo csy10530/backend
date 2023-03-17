8301aa53a66897f3e4d2d993a698d3a356e9025e
Siyuan Chen <chen.siyuan@northeastern.edu>
2023-03-16 23:36:13 -0400

    added cert

--- server.ts
+++ server.ts
@@ -13,20 +13,20 @@ const https = require("https")
 const connectDB = "mongodb+srv://usc_steven_chen:200810530@backend.iigppdj.mongodb.net/?retryWrites=true&w=majority";
 const frontend = "http://localhost:3000";
 
-const key = fs.readFileSync("key.pem")
-const cert = fs.readFileSync("server.crt")
-const credentials = {key: key, cert: cert}
-
 const cors = require("cors");
 
 const app = express();
-const httpsServer = https.createServer(credentials, app);
+/*const key = fs.readFileSync("key.pem")
+const cert = fs.readFileSync("server.crt")
+const credentials = {key: key, cert: cert}
+const httpsServer = https.createServer(credentials, app);*/
+const httpServer = createServer(app);
 app.use(bodyParser.json());
 
-app.use(cors(/*{
+app.use(cors({
     credentials: true,
     origin: [frontend, "https://ec2-34-232-65-71.compute-1.amazonaws.com:3000/", "https://main.d142bosvtdejd3.amplifyapp.com/"]
-}*/));
+}));
 
 mongoose.connect(connectDB);
 app.use(express.json());
@@ -38,4 +38,5 @@ const productController = ProductController.getInstance(app);
 AuthenticationController(app);
 
 const PORT = 4000;
-httpsServer.listen(process.env.PORT || PORT);
+//httpsServer.listen(process.env.PORT || PORT);
+httpServer.listen(process.env.PORT || PORT);
